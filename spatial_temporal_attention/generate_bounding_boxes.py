157ec0c81a4bea9b3b22f658509228e0d0d8e4df
LiliMeng <lilimeng1103@gmail.com>
2018-09-04 09:57:51 -0700

    add for loop for all the folders in generate bouding boxes and heatmap

--- spatial_temporal_attention/generate_bounding_boxes.py
+++ spatial_temporal_attention/generate_bounding_boxes.py
@@ -9,24 +9,9 @@ Date: Sep 3rd, 2018
 import numpy as np
 import cv2
 import os
-
+import shutil
 
 def generate_boundingbox(gray_img, threshold):
-    """Generate a bounding box for the heatmap"""
-    ret,th1 = cv2.threshold(gray_img, threshold, 255, cv2.THRESH_BINARY)
-    _, contours, hierarchy = cv2.findContours(th1, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    size = 0
-    x, y, w, h = 0, 0, 0, 0
-    for contour in contours:
-        x_, y_, w_, h_ = cv2.boundingRect( contour )
-        if w_ * h_ > size:
-            x, y, w, h = x_, y_, w_, h_
-            size = w * h
-
-    return [x, y, x, y]
-
-
-def generate_boundingbox1(gray_img, threshold):
 
 	result_gray_img_show = gray_img.copy()
 
@@ -36,8 +21,6 @@ def generate_boundingbox1(gray_img, threshold):
 
 	ret,th1 = cv2.threshold(result_gray_img_show, threshold, 255, cv2.THRESH_BINARY)
 	height, width = th1.shape[:2]
-	print("height: ", height)
-	print("width: ", width)
 	x1, y1, x2, y2 = width*2, height*2, 0, 0
 	for y in range(height):
 		for x in range(width):
@@ -51,25 +34,34 @@ def generate_boundingbox1(gray_img, threshold):
 		return [x1, y1, x2, y2]
 
 
-def draw_boundingbox_on_org_img():
+def draw_boundingbox_on_heatmap_img(gray_img_video_dir, heatmap_img_video_dir, boundingbox_dir, bbox_threshold):
 	
-	bbox_threshold = 190
-	gray_img_video_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test/v_Basketball_g01_c02'
-	old_heatmap_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test/old_v_Basketball_g01_c02'
-	boundingbox_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test_bbox/old_v_Basketball_g01_c02'
-
 	if not os.path.exists(boundingbox_dir):
 		os.makedirs(boundingbox_dir)
 
-	
-	for img_name in sorted(os.listdir(gray_img_video_dir)):
-		if '.jpg' in img_name:
-			gray_img = cv2.imread(os.path.join(gray_img_video_dir, img_name), 0)
-			pred_box = generate_boundingbox1(gray_img, bbox_threshold)
-			org_img = cv2.imread(os.path.join(old_heatmap_dir, img_name))
-			org_img_copy = org_img.copy()
-			cv2.rectangle(org_img_copy,(int(pred_box[0]),int(pred_box[1])),(int(pred_box[2]),int(pred_box[3])),(255,0,0),2)
-			cv2.imwrite(os.path.join(boundingbox_dir, img_name), org_img_copy)
+	for sub_dir in sorted(os.listdir(gray_img_video_dir)):
+		sub_gray_img_video_dir = os.path.join(gray_img_video_dir, sub_dir)
+		print("sub_gray_img_video_dir: ", sub_gray_img_video_dir)
+		sub_heatmap_img_video_dir = os.path.join(heatmap_img_video_dir, sub_dir)
+		sub_bounding_box_dir = os.path.join(boundingbox_dir, sub_dir)
+		assert(len(os.listdir(sub_gray_img_video_dir))==20)
+		assert(len(os.listdir(sub_heatmap_img_video_dir))==20)
+		if not os.path.exists(sub_bounding_box_dir):
+			os.makedirs(sub_bounding_box_dir)
+		for img_name in sorted(os.listdir(sub_gray_img_video_dir)):
+			assert(len(os.listdir(sub_gray_img_video_dir))==20)
+			if '.jpg' in img_name:
+				gray_img = cv2.imread(os.path.join(sub_gray_img_video_dir,  img_name), 0)
+				pred_box = generate_boundingbox(gray_img, bbox_threshold)
+				org_img = cv2.imread(os.path.join(sub_heatmap_img_video_dir, img_name))
+				org_img_copy = org_img.copy()
+				cv2.rectangle(org_img_copy,(int(pred_box[0]),int(pred_box[1])),(int(pred_box[2]),int(pred_box[3])),(255,0,0),2)
+				cv2.imwrite(os.path.join(sub_bounding_box_dir, img_name), org_img_copy)
 
 
-draw_boundingbox_on_org_img()
+gray_img_video_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test_gray'
+heatmap_img_video_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test_heatmap'
+boundingbox_dir = './mask_visualization/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02/test_bbox/'
+bbox_threshold = 190
+
+draw_boundingbox_on_heatmap_img(gray_img_video_dir, heatmap_img_video_dir, boundingbox_dir, bbox_threshold)
--- spatial_temporal_attention/generate_heatmap.py
+++ spatial_temporal_attention/generate_heatmap.py
@@ -8,63 +8,66 @@ import os
 import numpy as np
 import cv2
 
+def generate_heatmap(mask_dir, mode, use_heatmap):
+
+    mode_mask_dir = os.path.join(mask_dir, mode)
+
+    saved_vis_dir = os.path.join("./mask_visualization",mode_mask_dir)
+
+    if not os.path.exists(saved_vis_dir):
+        os.makedirs(saved_vis_dir)
+
+    video_name= np.load("./saved_weights/"+mask_dir+"/"+mode.split('_')[0]+"_name.npy")
+    video_weights = np.load("./saved_weights/"+mask_dir+"/"+mode.split('_')[0]+"_att_weights.npy")
+
+    video_name = np.concatenate(video_name, axis=0)
+
+    print("video_name.shape: ", video_name.shape)
+
+    video_frame_length = video_name.shape[1]
+    print("video_frame_length: ", video_frame_length)
+
+    for video_index in range(video_name.shape[0]):
+        single_video_name = video_name[video_index]
+        single_video_weights = video_weights[video_index].reshape(video_frame_length,7,7)
+
+
+        for img_indx in range(video_frame_length):
+            img_path = single_video_name[img_indx]
+            print('img_path: ', img_path)
+           
+            img = cv2.imread(img_path)
+            #height, width, _ = img.shape
+            img = cv2.resize(img, (256, 256))
+
+            cam = single_video_weights[img_indx]
+            cam = cam - np.min(cam)
+            cam_img = cam / np.max(cam)
+            cam_img = np.uint8(255 * cam_img)
+            cam_img = cv2.resize(cam_img, (256, 256))
+
+            if use_heatmap == True:
+                heatmap = cv2.applyColorMap(cam_img, cv2.COLORMAP_JET)
+                result = heatmap * 0.3 + img* 0.5
+            else:
+                result = cam_img
+
+
+            result_dir = os.path.join(saved_vis_dir, img_path.split('/')[-2])
+            result_name = img_path.split('/')[-1]
+            if not os.path.exists(result_dir):
+                os.makedirs(result_dir)
+            print("result_name: ", result_name)
+            cv2.imwrite(result_dir+'/'+result_name, result)
+
 
 mask_dir ="Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_03_17_02"
-mode = "test"
+mode = "test_heatmap"
 
-if mode == "train":
-    mode_mask_dir = os.path.join(mask_dir, mode)
-elif mode == "test":
-    mode_mask_dir = os.path.join(mask_dir, mode)
+if mode == "test_heatmap":
+    use_heatmap = True
+elif mode == "test_gray":
+    use_heatmap = False
 else:
-    raise Exception("no such mode, it shall be either train or test")
-
-video_index = 1
-
-video_frame_length = 20
-
-saved_vis_dir = os.path.join("./mask_visualization",mode_mask_dir)
-
-if not os.path.exists(saved_vis_dir):
-    os.makedirs(saved_vis_dir)
-
-video_name= np.load("./saved_weights/"+mask_dir+"/"+mode+"_name.npy")
-video_weights = np.load("./saved_weights/"+mask_dir+"/"+mode+"_att_weights.npy")
-
-print("video_name.shape: ", video_name.shape)
-print("train_weights.shape: ", video_weights.shape)
-
-
-video_name = np.concatenate(video_name, axis=0)
-
-print("video_name.shape: ", video_name.shape)
-print("train_weights.shape: ", video_weights.shape)
-
-single_video_name = video_name[video_index]
-single_video_weights = video_weights[video_index].reshape(video_frame_length,7,7)
-
-
-for img_indx in range(video_frame_length):
-    img_path = single_video_name[img_indx]
-    print('img_path: ', img_path)
-   
-    img = cv2.imread(img_path)
-    #height, width, _ = img.shape
-    img = cv2.resize(img, (256, 256))
-
-    cam = single_video_weights[img_indx]
-    cam = cam - np.min(cam)
-    cam_img = cam / np.max(cam)
-    cam_img = np.uint8(255 * cam_img)
-    cam_img = cv2.resize(cam_img, (256, 256))
-
-    #heatmap = cv2.applyColorMap(cam_img, cv2.COLORMAP_JET)
-    heatmap = cam_img
-    result = heatmap * 0.3 #+ img* 0.5
-
-    result_dir = os.path.join(saved_vis_dir, img_path.split('/')[-2])
-    result_name = img_path.split('/')[-1]
-    if not os.path.exists(result_dir):
-        os.makedirs(result_dir)
-    print("result_name: ", result_name)
-    cv2.imwrite(result_dir+'/'+result_name, result)
+    raise Exception("only test_heatmap and test_gray mode are provided now")
+generate_heatmap(mask_dir,mode, use_heatmap)
